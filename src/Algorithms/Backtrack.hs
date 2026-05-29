f87596a531692020abd35c7a333a6f9db9418041
felix <fdilke@gmail.com>
2026-05-29 20:43:06 +0100

    transformEithers utility

--- src/Algorithms/Backtrack.hs
+++ src/Algorithms/Backtrack.hs
@@ -1,20 +1,14 @@
-{-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# HLINT ignore "Unused LANGUAGE pragma" #-}
-{-# HLINT ignore "Use concatMap" #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
-{-# HLINT ignore "Move catMaybes" #-}
-{-# HLINT ignore "Use mapMaybe" #-}
-{-# HLINT ignore "Use list comprehension" #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE NoFieldSelectors #-}
-{-# LANGUAGE NoMonomorphismRestriction #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 module Algorithms.Backtrack
 where
+
+transformEithers :: forall a b . [Either a b] -> ([a], [b])
+transformEithers = foldr go ([], [])
+  where
+    go :: Either a b -> ([a], [b]) -> ([a], [b])
+    go (Left a) (as, bs) = (a:as, bs)
+    go (Right b) (as, bs) = (as, b:bs)
 
 solve :: forall a b. a -> (a -> [Either a b]) -> [b]
 solve start next = []
--- test/Test/Algorithms/BacktrackSpec.hs
+++ test/Test/Algorithms/BacktrackSpec.hs
@@ -1,17 +1,18 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-
 {-# HLINT ignore "Use const" #-}
 {-# HLINT ignore "Unused LANGUAGE pragma" #-}
 {-# HLINT ignore "Avoid lambda" #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 module Test.Algorithms.BacktrackSpec where
 
-import Test.Hspec
-import Test.Hspec.QuickCheck (prop)
 import Algorithms.Backtrack
+import Test.Hspec
 
 spec :: Spec
 spec = do
   describe "Backtracking" $ do
+    it "transforms a list of Eithers" $
+      transformEithers [Left (1 :: Int), Right "a", Left 2, Left 3, Right "b"] `shouldBe` (([1, 2, 3] :: [Int]), ["a", "b"])
     it "gives 0 solutions when there are none" $
       solve @Bool @Int True (\_ -> []) `shouldBe` []
